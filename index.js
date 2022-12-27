1f67dadca9bdf455be3f6860a03eb62c18901818
Shuvo Khan <shuvoprogramer@gmail.com>
2022-12-27 18:49:39 +0600

    filter skills

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express')
+// const http = require('http');
 const cors = require('cors')
 require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb')
 
 const port = process.env.PORT || 5000;
+// const httpServer = http.createServer(app);
+// const {Server} = require('socket.io');
+// const io = new Server(httpServer);
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// io.on("connection", (socket) => {
+//     console.log("new user connected to our app");
+
+//     socket.on("disconnect", (socket) => {
+//         console.log("user disconnect")
+//     })
+// })
+
 
 const uri = process.env.DB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -32,15 +45,34 @@ try {
         res.send(skills)
     })
 
-    app.get('/skills', async (req, res) => {
-        const skills = await skillsCollection.find().toArray();
+    app.get('/skills/expertise', async (req, res) => {
+        const query = {
+            track: {
+                $eq: "expertise"
+            }
+        }
+        const skills = await skillsCollection.find(query).toArray();
         res.send(skills);
     })
 
-    // app.post('/skills', async(req, res) => {
-    //     const skills = await skillsCollection.insertOne(req.body);
-    //     res.send(skills);
-    // })
+    app.get('/skills/comfortable', async (req, res) => {
+        const query = {
+            track: {
+                $eq: "comfortable"
+            }
+        }
+        const skills = await skillsCollection.find(query).toArray();
+        res.send(skills);
+    })
+    app.get('/skills/tools', async (req, res) => {
+        const query = {
+            track: {
+                $eq: "Tools"
+            }
+        }
+        const skills = await skillsCollection.find(query).toArray();
+        res.send(skills);
+    })
 } catch (error) {
     console.log(err)
 } finally {
